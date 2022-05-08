35a27cda47d1074732c722d84ea58a723a416a04
valeriyshashkin <velianjanjan@gmail.com>
2022-05-08 21:31:10 +0500

    better begin with materials than textures

---
+++ Assets/Scripts/HideCursor.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCursor : MonoBehaviour
+{
+    void Start() {
+        Cursor.visible = false;
+    }
+}
