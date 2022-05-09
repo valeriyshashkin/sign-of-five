76720d57cecd306f769236d2b0f7dc032bde9714
valeriyshashkin <velianjanjan@gmail.com>
2022-05-09 15:17:01 +0500

    disable mouse interaction at all

--- Assets/Scripts/HideCursor.cs
+++ Assets/Scripts/HideCursor.cs
@@ -1,10 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class HideCursor : MonoBehaviour
 {
-    void Start() {
+    GameObject lastSelected;
+
+    void Start()
+    {
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        lastSelected = new GameObject();
+    }
+
+    void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(lastSelected);
+        }
+        else
+        {
+            lastSelected = EventSystem.current.currentSelectedGameObject;
+        }
     }
 }
