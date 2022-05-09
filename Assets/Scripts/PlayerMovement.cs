b1f68fa093a0ce2fb1be2edf7094b1cb88d2586a
valeriyshashkin <velianjanjan@gmail.com>
2022-05-09 18:23:41 +0500

    move all logic for interactable from playermovement to yarninteractable

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,62 +7,16 @@ public class PlayerMovement : MonoBehaviour
 {
     CharacterController controller;
     DialogueRunner dialogueRunner;
-    Animator iconAnimator;
     Vector3 playerVelocity;
     float playerSpeed = 5.0f;
     float jumpHeight = 1.0f;
     float gravityValue = -9.81f;
     bool jump;
-    bool makeSomeAction;
-    bool inTrigger;
-
-    void OnTriggerEnter(Collider collider)
-    {
-        if (collider.tag == "Interactable")
-        {
-            iconAnimator = collider.gameObject.transform
-                .GetChild(0)
-                .gameObject.GetComponent<Animator>();
-            inTrigger = true;
-            iconAnimator.Play("FadeIn");
-        }
-    }
-
-    void OnTriggerExit(Collider collider)
-    {
-        if (collider.tag == "Interactable")
-        {
-            iconAnimator = collider.gameObject.transform
-                .GetChild(0)
-                .gameObject.GetComponent<Animator>();
-            inTrigger = false;
-            iconAnimator.Play("FadeOut");
-        }
-    }
-
-    void OnTriggerStay(Collider collider)
-    {
-        if (makeSomeAction)
-        {
-            iconAnimator.Play("FadeOut");
-            makeSomeAction = false;
-            collider.gameObject.GetComponent<YarnInteractable>().Interact();
-        }
-    }
-
-    void OnDialogueComplete()
-    {
-        if (inTrigger)
-        {
-            iconAnimator.Play("FadeIn");
-        }
-    }
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
         dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
-        dialogueRunner.onDialogueComplete.AddListener(OnDialogueComplete);
     }
 
     void Update()
@@ -70,11 +24,6 @@ public class PlayerMovement : MonoBehaviour
         if (dialogueRunner.IsDialogueRunning)
         {
             return;
-        }
-
-        if (Input.GetButtonUp("Submit") && !makeSomeAction)
-        {
-            makeSomeAction = true;
         }
 
         if (Input.GetButtonDown("Jump") && controller.isGrounded)
--- Assets/Scripts/YarnInteractable.cs
+++ Assets/Scripts/YarnInteractable.cs
@@ -6,14 +6,68 @@ using Yarn.Unity;
 public class YarnInteractable : MonoBehaviour
 {
     DialogueRunner dialogueRunner;
+    Animator animator;
     public string node;
+    bool userRequestAction;
+    bool inTrigger;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            inTrigger = true;
+            animator.Play("FadeIn");
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            inTrigger = false;
+            animator.Play("FadeOut");
+        }
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        if (userRequestAction)
+        {
+            userRequestAction = false;
+            animator.Play("FadeOut");
+            Interact();
+        }
+    }
+
+    void OnDialogueComplete()
+    {
+        if (inTrigger)
+        {
+            animator.Play("FadeIn");
+        }
+    }
 
     void Start()
     {
+        animator = transform.GetChild(0).GetComponent<Animator>();
         dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
+        dialogueRunner.onDialogueComplete.AddListener(OnDialogueComplete);
     }
 
-    public void Interact()
+    void Update()
+    {
+        if (dialogueRunner.IsDialogueRunning)
+        {
+            return;
+        }
+
+        if (Input.GetButtonUp("Submit") && inTrigger)
+        {
+            userRequestAction = true;
+        }
+    }
+
+    void Interact()
     {
         dialogueRunner.StartDialogue(node);
     }
