e7da2e312fd10b70c3d5c1b57421b661cccf7b11
valeriyshashkin <velianjanjan@gmail.com>
2022-05-23 13:44:10 +0500

    add quit command and rename characters into russian

---
+++ Assets/Scripts/GlobalCommands.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class GlobalCommands : MonoBehaviour
+{
+    void Start()
+    {
+        FindObjectOfType<Yarn.Unity.DialogueRunner>().AddCommandHandler("quit", () => Quit());
+    }
+
+    void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
